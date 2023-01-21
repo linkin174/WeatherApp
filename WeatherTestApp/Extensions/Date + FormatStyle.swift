5129194a129523d8c5eb421183685c0e8f19d735
Aleksandr Kretov <kretov.1988@gmail.com>
2023-01-21 13:27:47 +0500

    Added date formatting styles

--- WeatherTestApp/Extensions/Date + FormatStyle.swift
+++ WeatherTestApp/Extensions/Date + FormatStyle.swift
@@ -12,4 +12,26 @@ extension Date {
         let delta = timeInterval - Double(TimeZone.current.secondsFromGMT())
         return self.addingTimeInterval(delta).formatted(.dateTime.hour(.twoDigits(amPM: .omitted)).minute(.twoDigits))
     }
+
+    func hourAMPMStyle() -> String {
+        return self.formatted(.dateTime.hour(.defaultDigits(amPM: .abbreviated)))
+    }
+
+    func dayNameStyle() -> String {
+        self.formatted(.dateTime.weekday(.wide))
+    }
+
+    func dateFrom(string: String, timezoneShift: Double?) -> Self? {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        if let timezoneShift {
+            return formatter.date(from: string)?.addingTimeInterval(timezoneShift)
+        } else {
+            return formatter.date(from: string)
+        }
+    }
+
+    func dateFrom(secondsUTC: Int) -> Self {
+        Date(timeIntervalSince1970: Double(secondsUTC))
+    }
 }
